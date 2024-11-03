768a8ca519521b44b6e9a2ed81ec0addcb744a98
Jimoh Babatunde <qbabatunde@gmail.com>
2024-11-03 14:56:01 +0100

    Week 1 Assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("what is your name")
+age = input("what is your age")
+location = input("enter your location")
+
+print(f"Hello {name}, You are {age} years old and live in {location} ")
